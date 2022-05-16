6ef116a6aefa60b9ebef76264aa965ed16cf5cef
Andy Alex Carr <andresale.carrillo@outlook.com>
2022-05-16 16:39:08 -0300

    Add "computerPlay" function

---
+++ game-script.js
@@ -0,0 +1,15 @@
+function computerPlay() {
+    switch (Math.floor(Math.random()*3) + 1) {
+        case 1:
+            return "rock";
+            break;
+    
+        case 2:
+            return "paper";
+            break;
+        
+        case 3:
+            return "scissors";
+            break;
+    }
+}
